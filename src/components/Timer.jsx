774a765247f362f8565ce13a93383265107d104a
Sebastian Dragan <sebastian.dragan305@gmail.com>
2024-01-09 18:16:24 +0100

    add timer functionality

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import './Timer.scss';
 
 const Timer = () => {
-  return <div className="Timer">30</div>;
+  const [time, setTime] = useState(30);
+  const intervalIndexRef = useRef(null);
+
+  useEffect(() => {
+    intervalIndexRef.current = setInterval(() => {
+      setTime((prevValue) => {
+        if (prevValue === 1) clearInterval(intervalIndexRef.current);
+        return prevValue - 1;
+      });
+    }, 100);
+
+    return () => clearInterval(intervalIndexRef.current);
+  }, []);
+
+  return <div className="Timer">{time < 10 ? `0${time}` : time}</div>;
 };
 
 export default Timer;
